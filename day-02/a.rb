04af041e0a776b959933d91e15cb6be7e254e81c
Gareth Adams <g@rethada.ms>
2024-12-03 17:56:02 +0000

    [2] Extract report class to separate file

--- day-02/a.rb
+++ day-02/a.rb
@@ -1,28 +1,6 @@
 # frozen_string_literal: true
 
-class Report
-  def initialize(line)
-    @levels = line.split(/\s+/).map(&:to_i)
-  end
-
-  def error
-    diffs = @levels.each_cons(2).map { |a, b| b - a }
-    return :unordered unless diffs.map { |d| d / d.abs }.uniq.size == 1
-    return :jump unless diffs.all? { |d| d.abs <= 3 }
-
-    nil
-  rescue ZeroDivisionError
-    :duplicates
-  end
-
-  def safe?
-    !error
-  end
-
-  def to_s
-    @levels.to_s
-  end
-end
+require_relative 'report'
 
 count = 0
 
---
+++ day-02/report.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Report
+  def initialize(line)
+    @levels = line.split(/\s+/).map(&:to_i)
+  end
+
+  def error
+    diffs = @levels.each_cons(2).map { |a, b| b - a }
+    return :unordered unless diffs.map { |d| d / d.abs }.uniq.size == 1
+    return :jump unless diffs.all? { |d| d.abs <= 3 }
+
+    nil
+  rescue ZeroDivisionError
+    :duplicates
+  end
+
+  def safe?
+    !error
+  end
+
+  def to_s
+    @levels.to_s
+  end
+end
