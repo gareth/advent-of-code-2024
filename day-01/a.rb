d8f3b85c97ad553049d6b8a0f17f476a45474ab3
Gareth Adams <g@rethada.ms>
2024-12-03 17:16:53 +0000

    [1] Add solution A

---
+++ day-01/a.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+left = []
+right = []
+
+while (line = gets)
+  values = line.split(/\s+/)
+  left << values[0].to_i
+  right << values[1].to_i
+end
+
+puts left.sort.zip(right.sort).inject(0) { |memo, (l, r)| memo + (l - r).abs }
